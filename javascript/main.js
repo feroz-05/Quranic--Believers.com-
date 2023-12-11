b4104dbfeea78b932055dd4355e67710a45e2cf4
Firoz khan <mdkhanfiros@gmail.com>
2023-12-12 01:18:21 +0530

    added new page

--- javascript/main.js
+++ javascript/main.js
@@ -1,24 +1,49 @@
 
 import { namesOfSurah } from "../data/namesofsurah.js";
 let namesHTML ='';
-namesOfSurah.forEach((name)=>{
-    namesHTML +=`<div class="item">
-    <div class="sequence-ayahs">
-        ${name.number}
-    </div>
-    <div class="surah-name-english">
-        <div class="heading">${name.firstname}</div>
-        <div class="subtitle">${name.subname}</div>
-    </div>
-    <div class="surah-name-arabic">
-        <div class="heading-arabic">
-            ${name.image}
+
+//Generating Html code for the Each Button in container
+
+function generateHtml(){
+    namesOfSurah.forEach((name)=>{
+        namesHTML +=`<button class="button-item">
+        <div class="item">
+            <div class="sequence-ayahs">
+                ${name.number}
+            </div>
+            <div class="surah-name-english">
+                <div class="heading">${name.firstname}</div>
+                <div class="subtitle">${name.subname}</div>
+            </div>
+            <div class="surah-name-arabic">
+                <div class="heading-arabic">
+                    ${name.image}
+                </div>
+                <div class="subtitle-arabic">
+                    ${name.ayahs}
+                </div>
+            </div>
         </div>
-        <div class="subtitle-arabic">
-            ${name.ayahs}
-        </div>
-    </div>
-</div>`
-});
+        </button>`
+})};
+generateHtml();
 document.querySelector(".container").innerHTML=namesHTML;
+
+//Ends generating;
+
+const elements = document.querySelectorAll(".button-item");
+
+    elements.forEach((element)=>{
+        element.addEventListener('click', ()=>{
+            window.location.href="/html/surahpage.html";
+            console.log("working");
+    })
+    
+});
+
+
+
+
+
+
 
